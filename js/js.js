6813b477f480a121735fd53bbf53fed75b06c26e
htua <bobere229eva@gmail.com>
2026-05-21 12:36:07 +0300

    Delete js directory

--- js/js.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function () {
-    const header = document.getElementById('header');
-    if (window.scrollY > 0) {
-        header.classList.add('scroll');
-    } else {
-        header.classList.remove('scroll');
-    }
-});
